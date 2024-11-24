6e30bc78832e8dfa8b0c7f7c8ddc77b76aa1c61e
mahela-dev <mahela658@gmail.com>
2024-11-24 23:29:06 +0530

    initial commit

---
+++ src/main/java/com/example/ticketsystemest/TicketSystemestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketsystemest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSystemestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticketsystemest/TicketSystemestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketsystemest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
