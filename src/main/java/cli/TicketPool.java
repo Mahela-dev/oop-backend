16d83553e78872ba71f08eb872627042c47c024e
mahela-dev <mahela658@gmail.com>
2024-12-06 07:17:33 +0530

    configer class2

--- src/main/java/cli/TicketPool.java
+++ src/main/java/cli/TicketPool.java
@@ -5,6 +5,6 @@ public class TicketPool   extends TicketConfig{
 
 
     public TicketPool(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketsCapacity) {
-        super(totalTickets, ticketReleaseRate, customerRetrievalRate, maxTicketsCapacity);
+        super(totalTickets, ticketReleaseRate, customerRetrievalRate, maxTicketsCapacity) ;
     }
 }
