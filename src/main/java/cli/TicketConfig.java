0a8a4d1f1bb6d4b07542f27f386ffe0d091759f1
mahela-dev <mahela658@gmail.com>
2024-12-06 07:19:15 +0530

    configer class5

--- src/main/java/cli/TicketConfig.java
+++ src/main/java/cli/TicketConfig.java
@@ -32,43 +32,16 @@ public class TicketConfig {
         return maxTicketsCapacity;
     }
     public void setTotalTickets(int totalTickets) {
-        if (totalTickets < 0 || totalTickets > 1000) {
-            System.out.println("Invalid total ticket number");
-        }else {
             this.totalTickets = totalTickets;
-            System.out.println("Total ticket number: " + totalTickets);
-        }
     }
     public void setTicketReleaseRate(int ticketReleaseRate) {
-        if (totalTickets < 0 ) {
-            System.out.println("Invalid  ticket Release Rate");
-        } else if (ticketReleaseRate > totalTickets ) {
-            System.out.println("Invalid ticket release rate(Release rate can not exceed the total ticket)");
-        } else {
         this.ticketReleaseRate = ticketReleaseRate;
-            System.out.println("Ticket Release Rate set to : " + totalTickets);
-        }
     }
     public void setCustomerRetrievalRate(int customerRetrievalRate) {
-        if (totalTickets < 0 ) {
-            System.out.println("Invalid Retrieval Rate");
-        } else if (customerRetrievalRate > totalTickets ) {
-            System.out.println("Rate can not exceed the total ticket");
-        } else {
         this.customerRetrievalRate = customerRetrievalRate;
-            System.out.println("Customer ticket retrieval rate set to : " + totalTickets);
-        }
-
     }
     public void setMaxTicketsCapacity(int maxTicketsCapacity) {
-        if (totalTickets < 0) {
-            System.out.println("Invalid max Tickets Capacity");
-        } else if (maxTicketsCapacity > totalTickets ) {
-            System.out.println("Max Tickets Capacity can not exceed the total ticket");
-        } else {
         this.maxTicketsCapacity = maxTicketsCapacity;
-            System.out.println("Set max ticket capacity to : " + totalTickets);
-        }
     }
 
 public void saveToFile(String filename) {
--- src/main/java/cli/TicketPool.java
+++ src/main/java/cli/TicketPool.java
@@ -1,10 +1,65 @@
 package cli;
 
-public class TicketPool   extends TicketConfig{
+import java.util.List;
+
+public class TicketPool {
+  private TicketConfig ticketConfig;
+  private int releaseTicketCount;
+  private int buyTicketCount;
+  private final List<String> tickets;
+
+    public TicketPool(TicketConfig ticketConfig, int buyTicketCount, int releaseTicketCount, List<String> tickets) {
+        this.ticketConfig = ticketConfig;
+        this.tickets = tickets;
+        this.buyTicketCount = 0;
+        this.releaseTicketCount = 0;
+    }
+
+    public TicketConfig getTicketConfig() {
+        return ticketConfig;
+    }
+
+    public void setTicketConfig(TicketConfig ticketConfig) {
+        this.ticketConfig = ticketConfig;
+    }
+
+    public int getReleaseTicketCount() {
+        return releaseTicketCount;
+    }
+
+    public void setReleaseTicketCount(int releaseTicketCount) {
+        this.releaseTicketCount = releaseTicketCount;
+    }
+
+    public int getBuyTicketCount() {
+        return buyTicketCount;
+    }
+
+    public void setBuyTicketCount(int buyTicketCount) {
+        this.buyTicketCount = buyTicketCount;
+    }
+
+    public String addTicket(String vendor,int ticket) {
 
 
 
-    public TicketPool(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketsCapacity) {
-        super(totalTickets, ticketReleaseRate, customerRetrievalRate, maxTicketsCapacity) ;
+        if (ticket > ticketConfig.getTotalTickets()){
+            return "Ticket contity exceeds the maximum number of tickets";
+        }
+
+        for (int i = 0; i < ticketConfig.getTotalTickets(); i++) {
+            int ticketCount = this.tickets.size();
+
+            if (ticketCount > ticketConfig.getTotalTickets()){
+                String ticketData ="ticket number : "+tickets.size()+1 +"by vendor : "+vendor;
+                tickets.add(ticketData);
+            }else {
+                return "Released ticket reached the maximum number of tickets";
+            }
+        }
+        return ticket +"Ticket added to the pool by : "+vendor;
+    }
+    public void buyTicket(int ticket) {
+
     }
 }
